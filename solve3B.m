abd9b0dda71ccfc77cb991772714d162aa2121ec
gleyva1 <gleyva1@jhu.edu>
2022-09-27 18:52:13 -0600

    new file:   Gerard Leyva - Discussion 4 - Chaos.docx 	new file:   solve3B.m 	new file:   threeBf.m

---
+++ solve3B.m
@@ -0,0 +1,99 @@
+function S = solve3B(I,m1,m2,m3,G)
+    % Author: Gerard Leyva
+    % To run this, put in the command window >>
+    % I = [-0.5 0.5 0.5 0 -0.1 0.75 0 -0.3 0 0.3 0 -0.3]
+    % solve3B(I,0.5,0.5,0.5,0.0075)
+    f = 'threeBf';
+    t0 = 0;
+    n = 9500000;
+    %n = 100000;
+    hscale = 0.000015;
+
+    % Solar system simulation. These scales measures time in days. The
+    % other units are in usual SI.
+    % Type in console >>
+    % SSday = [0 0 1.495979e11 0 1.495979e11 -3.84472e8 0 0 0 2.592e9 8.83008e7 2.592e9]
+    % SEM = solve3B(SSday,1.9891e30,5.97219e24,7.34767309e22,0.498199);
+    %G = 0.498199;    % in m^3/(kg*day^2);
+    %m2 = 5.97219e24;    % Mass of the Earth (in kg)
+    %m3 = 7.34767309e22; % Mass of the Moon (in kg)
+    %m1 = 1.9891e30;     % Mass of the Sun (in kg)
+    %n = 5000000;
+    %hscale = 10^18;
+
+
+    % Runge-Kutta function to solve the ODE
+    %function R=rk4(t0,I,hscale,n,f)
+    function R=rk4(t0,I,hscale,n)
+        R=zeros(n+1,13);
+        t=zeros(n+1,1);
+        y=zeros(n+1,12);
+        t(1)=t0;
+        y(1,1)=I(1);      % x1
+        y(1,2)=I(2);      % y1
+        y(1,3)=I(3);      % x2
+        y(1,4)=I(4);      % y2
+        y(1,5)=I(5);      % x3
+        y(1,6)=I(6);      % y3
+        y(1,7)=I(7);      % vx1
+        y(1,8)=I(8);      % vy2
+        y(1,9)=I(9);      % vx2
+        y(1,10)=I(10);    % vy2
+        y(1,11)=I(11);    % vx3
+        y(1,12)=I(12);    % vy3
+
+        showpercentage = 1;
+        for j=1:n
+            r12 = sqrt((y(j,1)-y(j,3))^2 + (y(j,2)-y(j,4))^2);
+            r23 = sqrt((y(j,5)-y(j,3))^2 + (y(j,6)-y(j,4))^2);
+            r13 = sqrt((y(j,1)-y(j,5))^2 + (y(j,2)-y(j,6))^2);
+
+            h = hscale/(r12^2 + r23^2 + r13^2);
+            %h = hscale;
+
+            %{
+            %Original, 1D recipe
+            t(j+1)=t(j)+h;
+            k1=f(t(j),y(j));
+            k2=f(t(j)+h/2,y(j)+h/2*k1);
+            k3=f(t(j)+h/2,y(j)+h/2*k2);
+            k4=f(t(j)+h,y(j)+h*k3);
+            y(j+1)=y(j)+h/6*(k1+2*k2+2*k3+k4);
+            %}
+
+            % Modded for threeBf(I,m1,m2,m3,G)
+            t(j+1)=t(j)+h;
+            k1=threeBf(y(j,:),m1,m2,m3,G);
+            k2=threeBf(y(j,:)+h/2*k1,m1,m2,m3,G);
+            k3=threeBf(y(j,:)+h/2*k2,m1,m2,m3,G);
+            k4=threeBf(y(j,:)+h*k3,m1,m2,m3,G);
+            
+            y(j+1,:)=y(j,:)+h/6*(k1+2*k2+2*k3+k4);
+
+            if (100*j/n) > showpercentage
+                disp([num2str(showpercentage),'% complete (t = ',num2str(t(j+1)),')'])
+                showpercentage = showpercentage + 1;
+            end
+
+        end
+        R(:,1)=t;
+        R(:,2:13)=y;
+    end
+
+    S = rk4(t0,I,hscale,n);
+    
+    figure;close;hold on;
+    plot(S(:,2),S(:,3),'r',S(:,4),S(:,5),'b',S(:,6),S(:,7),'k');
+    %axis ([-3.5 3.5 -3.5 3.5]);
+    scatter(S(1,2),S(1,3),'rd','filled');
+    scatter(S(1,4),S(1,5),'bd','filled');
+    scatter(S(1,6),S(1,7),'kd','filled');
+    scatter(S(n+1,2),S(n+1,3),'r','filled');
+    scatter(S(n+1,4),S(n+1,5),'b','filled');
+    scatter(S(n+1,6),S(n+1,7),'k','filled');
+
+    xlabel('$x(t)$','interpreter','latex');
+    ylabel('$y(t)$','interpreter','latex');
+    title({['Three Body System: $m_{1}$ in red, $m_{2}$ in blue, $m_{3}$ in black.'];['Starting positions: Diamonds | Final positions: Dots']},'interpreter','latex');
+
+end
---
+++ threeBf.m
@@ -0,0 +1,73 @@
+function dot = threeBf(I,m1,m2,m3,G)
+    % Author: Gerard Leyva
+
+    % Input and output.
+    % Mass is entered through arguments m1,m2,m3. The G constant can be
+    % set to 1 to make the computation unitless.
+    %
+    % The input I is expected to be a Matlab vector, with positions being
+    % the first 6 elements (x1 position of mass 1, y1 position of mass 1,
+    % x2 position of mass 2, etc.), followed by velocities for the
+    % remaining six elements (vxi velocity of mass 1, vyi velocity of mass
+    % 1, etc.).
+    x1 = I(1);
+    y1 = I(2);
+    x2 = I(3);
+    y2 = I(4);
+    x3 = I(5);
+    y3 = I(6);
+    
+    dot = zeros(1,12);
+
+    % Physical parameters
+    %G = 6.67384e-11;    % SI units
+    %G = 0.498199;    % in m^3/(kg*day^2)
+    
+    %m2 = 5.97219e24;    % Mass of the Earth (in kg)
+    %m3 = 7.34767309e22; % Mass of the Moon (in kg)
+    %m1 = 1.9891e30;     % Mass of the Sun (in kg)
+
+    % Separation distances rij
+    r12 = sqrt((x1-x2)^2 + (y1-y2)^2);
+    r23 = sqrt((x3-x2)^2 + (y3-y2)^2);
+    r13 = sqrt((x1-x3)^2 + (y1-y3)^2);
+    
+    function Fij = gravF(mj,xi,xj,r)
+        Fij = -G*mj*(xi-xj)/r^3;
+    end
+
+    function Fijk = grav(mj,mk,xi,xj,xk,rij,rik)
+        Fijk = -G*mj*(xi-xj)/rij^3 - G*mk*(xi-xk)/rik^3;
+    end
+
+
+    % ODEs. First positions, then velocities
+    %{
+    dot(1) = I(7);  % xdot1 = vx1
+    dot(2) = I(8);  % ydot1 = vy1
+    dot(3) = I(9);  % xdot2 = vx2
+    dot(4) = I(10); % Ydot2 = vy2
+    dot(5) = I(11); % xdot3 = vx3
+    dot(6) = I(12); % ydot3 = vy3
+    dot(7) = gravF(m2,x1,x2,r12) + gravF(m3,x1,x3,r13);   % vxdot1
+    dot(8) = gravF(m2,y1,y2,r12) + gravF(m3,y1,y3,r13);   % vydot1
+    dot(9) = gravF(m1,x2,x1,r12) + gravF(m3,x2,x3,r23);   % vxdot2
+    dot(10) = gravF(m1,y2,y1,r12) + gravF(m3,y2,y3,r23);  % vydot2
+    dot(11) = gravF(m1,x3,x1,r13) + gravF(m2,x3,x2,r23);  % vxdot3
+    dot(12) = gravF(m1,y3,y1,r13) + gravF(m2,y3,y2,r23);  % vydot3
+    %}
+
+    dot(1) = I(7);  % xdot1 = vx1
+    dot(2) = I(8);  % ydot1 = vy1
+    dot(3) = I(9);  % xdot2 = vx2
+    dot(4) = I(10); % Ydot2 = vy2
+    dot(5) = I(11); % xdot3 = vx3
+    dot(6) = I(12); % ydot3 = vy3
+    dot(7) = grav(m2,m3,x1,x2,x3,r12,r13);   % vxdot1
+    dot(8) = grav(m2,m3,y1,y2,y3,r12,r13);   % vydot1
+    dot(9) = grav(m1,m3,x2,x1,x3,r12,r23);   % vxdot2
+    dot(10) = grav(m1,m3,y2,y1,y3,r12,r23);  % vydot2
+    dot(11) = grav(m1,m2,x3,x1,x2,r13,r23);  % vxdot3
+    dot(12) = grav(m1,m2,y3,y1,y2,r13,r23);  % vydot3
+
+end
